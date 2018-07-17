15f50b3a1144dfbf16d121b4ba8dff3c4a63f67f
David E Watson <davidewatson@gmail.com>
2018-07-17 15:21:04 -0700

    Correct providerconfig imports (fixes #1).

--- cloud/skeleton/providerconfig/v1alpha1/register.go
+++ cloud/skeleton/providerconfig/v1alpha1/register.go
@@ -22,7 +22,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/serializer"
 	clusterv1 "sigs.k8s.io/cluster-api/pkg/apis/cluster/v1alpha1"
 
-	"sigs.k8s.io/cluster-api-provider-skeleton/cloud/aws/providerconfig"
+	"sigs.k8s.io/cluster-api-provider-skeleton/cloud/skeleton/providerconfig"
 )
 
 // +k8s:deepcopy-gen=false

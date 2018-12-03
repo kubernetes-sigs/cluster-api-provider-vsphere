6f68be5435e65b07e443a0f86cd1843eb00c5c5d
Fabio Rapposelli <fabio@vmware.com>
2018-12-03 23:46:57 +0100

    Fix .gitignore under /installer and add missing scripts

---
+++ installer/build/bootable/root/opt/ova/bin/kubeadm-provisioning.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/bash
+
+# Fetch current local Kubernetes version
+KUBEVERSION=$(kubectl version --client -o json | jq -r .clientVersion.gitVersion)
+DCUI_SOCKET="UNIX:/var/run/dcui.sock"
+
+function log_to_dcui() {
+  local msg=$1
+  echo -n "$msg" | socat $DCUI_SOCKET -
+}
+
+function check_api_endpoint() {
+  local interval=$1
+  local max_retries=$2
+  local is_ready=$3
+  local retries=0
+  local JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'
+  while [ $retries -lt "$max_retries" ]; do
+    if kubectl --kubeconfig=/etc/kubernetes/admin.conf get nodes -o jsonpath="$JSONPATH" | grep "Ready=${is_ready}"; then
+      return 0
+    fi
+    (( retries=retries+1 ))
+    sleep "$interval"
+  done
+  echo "max retry has passed... exiting..."
+  exit 1
+}
+
+
+# Pull images
+log_to_dcui "[PULLING IMAGES](fg-yellow)"
+kubeadm config images pull --kubernetes-version "$KUBEVERSION"
+log_to_dcui "[INITIALIZING](fg-yellow)"
+kubeadm init --pod-network-cidr=10.244.0.0/16 --kubernetes-version "$KUBEVERSION"
+
+# Wait for API server to respond
+log_to_dcui "[STARTING](fg-yellow)"
+# Check if the node is up and Not Ready
+check_api_endpoint 1 100 False
+
+# Deploy flannel
+log_to_dcui "[DEPLOYING NETWORK](fg-yellow)"
+kubectl --kubeconfig=/etc/kubernetes/admin.conf apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
+
+log_to_dcui "[CONFIGURING](fg-yellow)"
+# Set up kubeconfig for root user
+mkdir -p "/root/.kube"
+cp -i /etc/kubernetes/admin.conf "/root/.kube/config"
+chown "$(id -u)":"$(id -g)" "/root/.kube/config"
+
+# Check if the node is up and Ready
+check_api_endpoint 1 100 True
+log_to_dcui "[RUNNING](fg-green)"
+
+# Set kubernetes provisioning datetime
+date -u +"%Y-%m-%dT%H:%M:%SZ" > /etc/vmware/kubeadm.provisioned

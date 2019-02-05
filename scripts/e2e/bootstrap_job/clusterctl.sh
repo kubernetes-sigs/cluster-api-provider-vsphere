c2b470d8891d27b6e4b10bc7323d7380728ad302
hui luo <luohui925@gmail.com>
2019-02-04 16:31:25 -0800

    use MachineList in machine yaml (#193)
    the current yaml example does not work with kubectl
    add MachineList make it a valid spec for kubectl

--- scripts/e2e/bootstrap_job/clusterctl.sh
+++ scripts/e2e/bootstrap_job/clusterctl.sh
@@ -41,7 +41,7 @@ do
    fi
 done
 wget https://storage.googleapis.com/kubernetes-release/release/v1.10.2/bin/linux/amd64/kubectl \
-     -O /usr/local/bin/kubectl
+     -nv -O /usr/local/bin/kubectl
 chmod +x /usr/local/bin/kubectl
 
 # run clusterctl

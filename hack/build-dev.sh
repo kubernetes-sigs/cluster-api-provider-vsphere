c0679b7709e921f13531e7cfeab6ca473687dd6e
akutz <akutz@vmware.com>
2019-09-01 12:08:09 -0500

    Add script for building dev artifacts
    This patch adds the script "hack/build-dev.sh" that:
    
    * Builds the CAPV manager image from the local, working copy of CAPV
      and pushes the image to
      "gcr.io/cluster-api-provider-vsphere/dev/v1alpha2/capv-manager:latest"
    
    * Builds the CAPV manifests image from the local, working copy of CAPV
      and pushes the image to
      "gcr.io/cluster-api-provider-vsphere/dev/v1alpha2/capv-manifests:latest"
    
    * Builds the CAPI manager image from its upstream repository's "master"
      branch and pushes the image to to
      "gcr.io/cluster-api-provider-vsphere/dev/v1alpha2/capi-manager:latest"
    
    * Builds the CABPK manager image from its upstream repository's "master"
      branch and pushes the image to to
      "gcr.io/cluster-api-provider-vsphere/dev/v1alpha2/cabpk-manager:latest"

---
+++ hack/build-dev.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# Copyright 2019 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# TODO(akutz) This script can probably be removed once v1alpha2 is released.
+#
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Change directories to the parent directory of the one in which this
+# script is located.
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
+
+export CGO_ENABLED=0
+export GOFLAGS="-ldflags=-extldflags=-static"
+export GOPROXY="${GOPROXY:-https://proxy.golang.org}"
+
+CLUSTERCTL_OUT="${CLUSTERCTL_OUT:-$(pwd)/clusterctl}"
+CAPV_MANAGER_IMAGE="${CAPV_MANAGER_IMAGE:-gcr.io/cluster-api-provider-vsphere/dev/v1alpha2/capv-manager:latest}"
+CAPV_MANIFEST_IMAGE="${CAPV_MANIFEST_IMAGE:-gcr.io/cluster-api-provider-vsphere/dev/v1alpha2/capv-manifests:latest}"
+CAPI_MANAGER_IMAGE="${CAPI_MANAGER_IMAGE:-gcr.io/cluster-api-provider-vsphere/dev/v1alpha2/capi-manager:latest}"
+CABPK_MANAGER_IMAGE="${CABPK_MANAGER_IMAGE:-gcr.io/cluster-api-provider-vsphere/dev/v1alpha2/cabpk-manager:latest}"
+
+# Build the CAPV manager image.
+docker build -t "${CAPV_MANAGER_IMAGE}" .
+docker push "${CAPV_MANAGER_IMAGE}"
+
+# Build the CAPV manifest image.
+docker build \
+  --build-arg "CAPV_MANAGER_IMAGE=${CAPV_MANAGER_IMAGE}" \
+  -t "${CAPV_MANIFEST_IMAGE}" \
+  -f hack/tools/generate-yaml/Dockerfile \
+  .
+docker push "${CAPV_MANIFEST_IMAGE}"
+
+# Create a temporary directory into which the CAPI and CABPK repos can
+# be cloned.
+cd "$(mktemp -d)"
+
+# Clone the CAPI and CABPK repositories.
+git clone https://github.com/kubernetes-sigs/cluster-api.git capi
+git clone https://github.com/kubernetes-sigs/cluster-api-bootstrap-provider-kubeadm cabpk
+
+# Switch to the CAPI repo.
+pushd capi
+
+# Build clusterctl
+go build -o "${CLUSTERCTL_OUT}" ./cmd/clusterctl
+
+# Build the CAPI manager image.
+docker build -t "${CAPI_MANAGER_IMAGE}" .
+docker push "${CAPI_MANAGER_IMAGE}"
+
+# Switch to the CABPK repo.
+popd && pushd cabpk
+
+# Build the CABPK manager image.
+docker build -t "${CABPK_MANAGER_IMAGE}" .
+docker push "${CABPK_MANAGER_IMAGE}"
+
+cat <<EOF
+
+clusterctl     ${CLUSTERCTL_OUT}
+capi_manager   ${CAPI_MANAGER_IMAGE}
+cabpk_manager  ${CABPK_MANAGER_IMAGE}
+capv_manager   ${CAPV_MANAGER_IMAGE}
+capv_manifests ${CAPV_MANIFEST_IMAGE}
+EOF

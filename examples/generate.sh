eea0b903f0154ae38b290567d1c3fc45379bf876
Andrew Sy Kim <kiman@vmware.com>
2019-09-24 12:16:40 -0400

    update CAPI manager version in manifest examples to v0.2.2
    Signed-off-by: Andrew Sy Kim <kiman@vmware.com>

--- examples/generate.sh
+++ examples/generate.sh
@@ -31,7 +31,7 @@ CLUSTER_NAME="${CLUSTER_NAME:-capv-mgmt-example}"
 ENV_VAR_REQ=':?required'
 
 CABPK_MANAGER_IMAGE="${CABPK_MANAGER_IMAGE:-us.gcr.io/k8s-artifacts-prod/capi-kubeadm/cluster-api-kubeadm-controller:v0.1.0}"
-CAPI_MANAGER_IMAGE="${CAPI_MANAGER_IMAGE:-us.gcr.io/k8s-artifacts-prod/cluster-api/cluster-api-controller:v0.2.1}"
+CAPI_MANAGER_IMAGE="${CAPI_MANAGER_IMAGE:-us.gcr.io/k8s-artifacts-prod/cluster-api/cluster-api-controller:v0.2.2}"
 CAPV_MANAGER_IMAGE="${CAPV_MANAGER_IMAGE:-gcr.io/cluster-api-provider-vsphere/release/manager:latest}"
 
 # Set the default log levels for the manager containers.

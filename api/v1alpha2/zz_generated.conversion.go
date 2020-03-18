5bdab22f7ee5a8aeac356370a6ce67836af4dbd4
Yassine TIJANI <ytijani@vmware.com>
2020-03-18 11:58:13 +0100

    Set network interface name with cloud-init set-name
    Signed-off-by: Yassine TIJANI <ytijani@vmware.com>
    
    Co-authored-by: Greg May <mrgregmay@gmail.com>

--- api/v1alpha2/zz_generated.conversion.go
+++ api/v1alpha2/zz_generated.conversion.go
@@ -672,6 +672,7 @@ func Convert_v1alpha2_NetworkDeviceSpec_To_v1alpha3_NetworkDeviceSpec(in *Networ
 
 func autoConvert_v1alpha3_NetworkDeviceSpec_To_v1alpha2_NetworkDeviceSpec(in *v1alpha3.NetworkDeviceSpec, out *NetworkDeviceSpec, s conversion.Scope) error {
 	out.NetworkName = in.NetworkName
+	// WARNING: in.DeviceName requires manual conversion: does not exist in peer-type
 	out.DHCP4 = in.DHCP4
 	out.DHCP6 = in.DHCP6
 	out.Gateway4 = in.Gateway4
@@ -715,7 +716,17 @@ func Convert_v1alpha3_NetworkRouteSpec_To_v1alpha2_NetworkRouteSpec(in *v1alpha3
 }
 
 func autoConvert_v1alpha2_NetworkSpec_To_v1alpha3_NetworkSpec(in *NetworkSpec, out *v1alpha3.NetworkSpec, s conversion.Scope) error {
-	out.Devices = *(*[]v1alpha3.NetworkDeviceSpec)(unsafe.Pointer(&in.Devices))
+	if in.Devices != nil {
+		in, out := &in.Devices, &out.Devices
+		*out = make([]v1alpha3.NetworkDeviceSpec, len(*in))
+		for i := range *in {
+			if err := Convert_v1alpha2_NetworkDeviceSpec_To_v1alpha3_NetworkDeviceSpec(&(*in)[i], &(*out)[i], s); err != nil {
+				return err
+			}
+		}
+	} else {
+		out.Devices = nil
+	}
 	out.Routes = *(*[]v1alpha3.NetworkRouteSpec)(unsafe.Pointer(&in.Routes))
 	out.PreferredAPIServerCIDR = in.PreferredAPIServerCIDR
 	return nil
@@ -727,7 +738,17 @@ func Convert_v1alpha2_NetworkSpec_To_v1alpha3_NetworkSpec(in *NetworkSpec, out *
 }
 
 func autoConvert_v1alpha3_NetworkSpec_To_v1alpha2_NetworkSpec(in *v1alpha3.NetworkSpec, out *NetworkSpec, s conversion.Scope) error {
-	out.Devices = *(*[]NetworkDeviceSpec)(unsafe.Pointer(&in.Devices))
+	if in.Devices != nil {
+		in, out := &in.Devices, &out.Devices
+		*out = make([]NetworkDeviceSpec, len(*in))
+		for i := range *in {
+			if err := Convert_v1alpha3_NetworkDeviceSpec_To_v1alpha2_NetworkDeviceSpec(&(*in)[i], &(*out)[i], s); err != nil {
+				return err
+			}
+		}
+	} else {
+		out.Devices = nil
+	}
 	out.Routes = *(*[]NetworkRouteSpec)(unsafe.Pointer(&in.Routes))
 	out.PreferredAPIServerCIDR = in.PreferredAPIServerCIDR
 	return nil

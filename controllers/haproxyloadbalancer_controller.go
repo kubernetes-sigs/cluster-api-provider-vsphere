57a50ebac8d757e5af5062c9127cfeb2f4103564
Fabio Rapposelli <fabio@vmware.com>
2020-03-23 11:44:13 +0100

    disable ipv6 in haproxy controller

--- controllers/haproxyloadbalancer_controller.go
+++ controllers/haproxyloadbalancer_controller.go
@@ -33,6 +33,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	apitypes "k8s.io/apimachinery/pkg/types"
+	utilnet "k8s.io/utils/net"
 	"k8s.io/utils/pointer"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1alpha3"
 	clusterutilv1 "sigs.k8s.io/cluster-api/util"
@@ -359,6 +360,10 @@ func (r haproxylbReconciler) BackEndpointsForCluster(ctx *context.HAProxyLoadBal
 		machineEndpoints := make([]corev1.EndpointAddress, 0)
 		for i, addr := range machine.Status.Addresses {
 			if addr.Type == clusterv1.MachineExternalIP {
+				// TODO(frapposelli): Remove this check once HAproxy fully supports IPv6 - issue #859
+				if utilnet.IsIPv6String(addr.Address) {
+					continue
+				}
 				endpoint := corev1.EndpointAddress{
 					NodeName: pointer.StringPtr(fmt.Sprintf("%s-%d", machine.Name, i)),
 					IP:       addr.Address,

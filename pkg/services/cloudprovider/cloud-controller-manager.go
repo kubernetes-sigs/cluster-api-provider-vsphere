a076f51ce25a177f453637ce4aca0e3508d482ea
Andrew Sy Kim <kim.andrewsy@gmail.com>
2020-08-26 12:46:16 -0400

    cloud provider: remove SKIP_NODE_DELETION config
    Signed-off-by: Andrew Sy Kim <kim.andrewsy@gmail.com>

--- pkg/services/cloudprovider/cloud-controller-manager.go
+++ pkg/services/cloudprovider/cloud-controller-manager.go
@@ -19,7 +19,6 @@ package cloudprovider
 import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	v1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -134,13 +133,6 @@ func CloudControllerManagerDaemonSet(image string, args []string) *appsv1.Daemon
 							Name:  "vsphere-cloud-controller-manager",
 							Image: image,
 							Args:  args,
-							Env: []v1.EnvVar{
-								{
-									// skips node deletion, remove when kubernetes/cloud-provider-vsphere#381 is resolved
-									Name:  "SKIP_NODE_DELETION",
-									Value: "true",
-								},
-							},
 							VolumeMounts: []corev1.VolumeMount{
 								{
 									Name:      "vsphere-config-volume",

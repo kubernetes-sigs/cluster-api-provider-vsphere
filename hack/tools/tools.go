9477ea0c4bc923dff6a9533fbca597ed89c23d06
Shreya Dubey <shdubey@vmware.com>
2022-01-26 18:09:55 -0800

    Align CAPV Makefile targets with CAPI and update the code
    - verify-boilerplate: updated the Copyright section in the required files
    - verify-gen: added this target to verfiy go generated files are up to date
    - verify-modules: added this target to verify go modules are up to date

--- hack/tools/tools.go
+++ hack/tools/tools.go
@@ -1,6 +1,3 @@
-//go:build tools
-// +build tools
-
 /*
 Copyright 2019 The Kubernetes Authors.
 
@@ -16,6 +13,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+//go:build tools
+// +build tools
 
 // This package imports things required by build scripts, to force `go mod` to see them as dependencies
 package tools

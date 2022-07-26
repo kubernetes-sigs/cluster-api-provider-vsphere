364052c02f601fffac55584707fd77da7dd0de9f
akutz <akutz@vmware.com>
2022-07-26 12:07:11 -0500

    Reuse CAPI bootstrap secret with Supervisor
    This patch removes the logic for creating a new ConfigMap
    resource for Supervisor VMs. Instead CAPV simply points
    the Supervisor resources to the existing CAPI bootstrap
    secrets.
    
    Previously Supervisor integration required setting
    the ControlPlaneEndpoint value in Cloud-Init metadata.
    However, this was recognized as vestigial as the
    KubeadmBootstrap controller automatically grabs the
    ControlPlaneEndpoint from the
    Cluster.Spec.ControlPlaneEndpoint field if the value is
    not explicitly specified.
    
    For more information please see the following links:
    
    * The CAPV Cluster reconciler does not proceed until there is a control plane endpoint -- https://github.com/kubernetes-sigs/cluster-api-provider-vsphere/blob/cc735927f7c7671dc8f30bc796474fa62555a127/controllers/vmware/vspherecluster_reconciler.go#L184-L188
    * The CAPV Cluster reconciler only marks the Cluster as "ready" when there is a valid control plane endpoint -- https://github.com/kubernetes-sigs/cluster-api-provider-vsphere/blob/cc735927f7c7671dc8f30bc796474fa62555a127/controllers/vmware/vspherecluster_reconciler.go#L190
    * The CAPI Core KubeadmBootstrap reconciler does not proceed unless the Cluster is marked as "ready" -- https://github.com/kubernetes-sigs/cluster-api/blob/bc177c6906e8b7063520777c2d608b7297fd86b9/bootstrap/kubeadm/internal/controllers/kubeadmconfig_controller.go#L234-L238
    * The CAPI Core KubeadmBootstrap reconciler uses the value of Cluster.Spec.ControlPlaneEndpoint if one is not provided explicitly -- https://github.com/kubernetes-sigs/cluster-api/blob/ffee0a45cf823965d6e6d578d93eb3b6730ec734/bootstrap/kubeadm/internal/controllers/kubeadmconfig_controller.go#L925-L931
    * The CAPV Machine reconciler does not proceed until the bootstrap data is ready -- https://github.com/kubernetes-sigs/cluster-api-provider-vsphere/blob/cc735927f7c7671dc8f30bc796474fa62555a127/controllers/vspheremachine_controller.go#L292-L302
    
    (cherry picked from commit 4ca52b0ab338cc208b49688c825a7555818f5622)

--- pkg/util/vmware/util.go
+++ pkg/util/vmware/util.go
@@ -17,54 +17,13 @@ limitations under the License.
 package vmware
 
 import (
-	"context"
-	"encoding/base64"
 	"fmt"
 
-	"github.com/pkg/errors"
-	corev1 "k8s.io/api/core/v1"
-	apitypes "k8s.io/apimachinery/pkg/types"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // GetMachineDeploymentName returns the MachineDeployment name for a Cluster.
 // This is also the name used by VSphereMachineTemplate and KubeadmConfigTemplate.
 func GetMachineDeploymentNameForCluster(cluster *clusterv1.Cluster) string {
 	return fmt.Sprintf("%s-workers-0", cluster.Name)
-}
-
-// GetBootstrapConfigMapName returns the name of the bootstrap data ConfigMap
-// for a VM Operator VirtualMachine.
-func GetBootstrapConfigMapName(machineName string) string {
-	return fmt.Sprintf("%s-cloud-init", machineName)
-}
-
-func GetBootstrapData(ctx context.Context, c client.Client, machine *clusterv1.Machine) (string, error) {
-	value, err := GetRawBootstrapData(ctx, c, machine)
-	if err != nil {
-		return "", err
-	}
-	return base64.StdEncoding.EncodeToString(value), nil
-}
-
-// GetRawBootstrapData returns the bootstrap data from the secret in the
-// Machine's bootstrap.dataSecretName.
-func GetRawBootstrapData(ctx context.Context, c client.Client, machine *clusterv1.Machine) ([]byte, error) {
-	if machine.Spec.Bootstrap.DataSecretName == nil {
-		return nil, errors.New("error retrieving bootstrap data: linked Machine's bootstrap.dataSecretName is nil")
-	}
-
-	secret := &corev1.Secret{}
-	key := apitypes.NamespacedName{Namespace: machine.GetNamespace(), Name: *machine.Spec.Bootstrap.DataSecretName}
-	if err := c.Get(ctx, key, secret); err != nil {
-		return nil, errors.Wrapf(err, "failed to retrieve bootstrap data secret for Machine %s/%s", machine.GetNamespace(), machine.GetName())
-	}
-
-	value, ok := secret.Data["value"]
-	if !ok {
-		return nil, errors.New("error retrieving bootstrap data: secret value key is missing")
-	}
-
-	return value, nil
 }

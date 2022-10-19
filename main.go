a784ce503bfa54faf6d9703540909281b3d43169
Sagar Muchhal <muchhals@vmware.com>
2022-10-18 17:19:55 -0700

    Set default min tls version to 1.2
    Signed-off-by: Sagar Muchhal <muchhals@vmware.com>

--- main.go
+++ main.go
@@ -63,6 +63,7 @@ var (
 	defaultWebhookPort       = manager.DefaultWebhookServiceContainerPort
 	defaultEnableKeepAlive   = constants.DefaultEnableKeepAlive
 	defaultKeepAliveDuration = constants.DefaultKeepAliveDuration
+	defaultTLSMinVersion     = "1.2"
 )
 
 // InitFlags initializes the flags.
@@ -147,7 +148,7 @@ func InitFlags(fs *pflag.FlagSet) {
 	flag.StringVar(
 		&tlsMinVersion,
 		"tls-min-version",
-		"",
+		defaultTLSMinVersion,
 		"minimum TLS version in use by the webhook server. Possible values are  \"\", \"1.0\", \"1.1\", \"1.2\" and \"1.3\".",
 	)
 

45e0518cb1b51b8e20400fd5f871c75e6077efcc
Sagar Muchhal <muchhals@vmware.com>
2023-03-20 17:59:01 -0700

    Updates kube-vip to v0.5.11
    Signed-off-by: Sagar Muchhal <muchhals@vmware.com>

--- packaging/flavorgen/flavors/generators.go
+++ packaging/flavorgen/flavors/generators.go
@@ -479,7 +479,7 @@ func kubeVIPPodSpec() *corev1.Pod {
 			Containers: []corev1.Container{
 				{
 					Name:            "kube-vip",
-					Image:           "ghcr.io/kube-vip/kube-vip:v0.5.5",
+					Image:           "ghcr.io/kube-vip/kube-vip:v0.5.11",
 					ImagePullPolicy: corev1.PullIfNotPresent,
 					Args: []string{
 						"manager",

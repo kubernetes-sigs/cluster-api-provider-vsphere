7b11c4ab2e0327d87e003da0b66ec473baef03e0
Ricardo Katz <rkatz@vmware.com>
2023-10-26 09:08:35 -0300

    Use a less expensive method for storage policy

--- pkg/services/govmomi/service_test.go
+++ pkg/services/govmomi/service_test.go
@@ -18,11 +18,14 @@ package govmomi
 
 import (
 	goctx "context"
+	"fmt"
 	"testing"
 
 	"github.com/go-logr/logr"
 	. "github.com/onsi/gomega"
 	"github.com/vmware/govmomi/find"
+	"github.com/vmware/govmomi/object"
+	pbmsimulator "github.com/vmware/govmomi/pbm/simulator"
 	"github.com/vmware/govmomi/simulator"
 	"github.com/vmware/govmomi/vim25"
 	"github.com/vmware/govmomi/vim25/types"
@@ -32,6 +35,11 @@ import (
 
 	infrav1 "sigs.k8s.io/cluster-api-provider-vsphere/apis/v1beta1"
 	"sigs.k8s.io/cluster-api-provider-vsphere/pkg/context"
+	"sigs.k8s.io/cluster-api-provider-vsphere/pkg/session"
+)
+
+const (
+	defaultStoragePolicy = "vSAN Default Storage Policy"
 )
 
 func emptyVirtualMachineContext() *virtualMachineContext {
@@ -95,9 +103,136 @@ func Test_reconcilePCIDevices(t *testing.T) {
 			pciDevices := devices.SelectByBackingInfo(&types.VirtualPCIPassthroughDynamicBackingInfo{
 				AllowedDevice: []types.VirtualPCIPassthroughAllowedDevice{
 					{DeviceId: 1234, VendorId: 5678},
-				}})
+				},
+			})
 			g.Expect(pciDevices).To(HaveLen(2))
 			return nil
 		})
 	})
+}
+
+func Test_ReconcileStoragePolicy(t *testing.T) {
+	var vmCtx *virtualMachineContext
+	var g *WithT
+	var vms *VMService
+
+	before := func() {
+		vmCtx = emptyVirtualMachineContext()
+		vmCtx.Client = fake.NewClientBuilder().Build()
+
+		vms = &VMService{}
+	}
+	t.Run("when VM has no storage policy spec", func(t *testing.T) {
+		g = NewWithT(t)
+		before()
+		vmCtx.VSphereVM = &infrav1.VSphereVM{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "vsphereVM1",
+				Namespace: "my-namespace",
+			},
+			Spec: infrav1.VSphereVMSpec{
+				VirtualMachineCloneSpec: infrav1.VirtualMachineCloneSpec{},
+			},
+		}
+		g.Expect(vms.reconcileStoragePolicy(vmCtx)).ToNot(HaveOccurred())
+		g.Expect(vmCtx.VSphereVM.Status.TaskRef).To(BeEmpty())
+	})
+
+	t.Run("when the requested storage policy does not exists should fail", func(t *testing.T) {
+		g = NewWithT(t)
+		before()
+		model, err := storagePolicyModel()
+		g.Expect(err).ToNot(HaveOccurred())
+
+		simulator.Run(func(ctx goctx.Context, c *vim25.Client) error {
+			authSession, err := getAuthSession(ctx, model.Service.Listen.Host)
+			g.Expect(err).ToNot(HaveOccurred())
+			vmCtx.Session = authSession
+			vm, err := getPoweredoffVM(ctx, c)
+			g.Expect(err).ToNot(HaveOccurred())
+
+			vmCtx.Obj = vm
+			vmCtx.VSphereVM = &infrav1.VSphereVM{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "vsphereVM1",
+					Namespace: "my-namespace",
+				},
+				Spec: infrav1.VSphereVMSpec{
+					VirtualMachineCloneSpec: infrav1.VirtualMachineCloneSpec{
+						StoragePolicyName: "non-existing-storagepolicy",
+					},
+				},
+			}
+			err = vms.reconcileStoragePolicy(vmCtx)
+			g.Expect(err.Error()).To(ContainSubstring("no pbm profile found with name"))
+			return nil
+		}, model)
+	})
+
+	t.Run("when the requested storage policy exists should pass", func(t *testing.T) {
+		// This Method should be implemented on Govmomi vcsim and then we can unskip this test
+		t.Skip("PbmQueryAssociatedProfiles is not yet implemented on PBM simulator")
+		g = NewWithT(t)
+		before()
+		model, err := storagePolicyModel()
+		g.Expect(err).ToNot(HaveOccurred())
+
+		simulator.Run(func(ctx goctx.Context, c *vim25.Client) error {
+			authSession, err := getAuthSession(ctx, model.Service.Listen.Host)
+			g.Expect(err).ToNot(HaveOccurred())
+			vmCtx.Session = authSession
+			vm, err := getPoweredoffVM(ctx, c)
+			g.Expect(err).ToNot(HaveOccurred())
+
+			vmCtx.Obj = vm
+			vmCtx.VSphereVM = &infrav1.VSphereVM{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "vsphereVM1",
+					Namespace: "my-namespace",
+				},
+				Spec: infrav1.VSphereVMSpec{
+					VirtualMachineCloneSpec: infrav1.VirtualMachineCloneSpec{
+						StoragePolicyName: defaultStoragePolicy,
+					},
+				},
+			}
+			err = vms.reconcileStoragePolicy(vmCtx)
+			g.Expect(err).ToNot(HaveOccurred())
+			return nil
+		}, model)
+	})
+}
+
+func getAuthSession(ctx goctx.Context, server string) (*session.Session, error) {
+	password, _ := simulator.DefaultLogin.Password()
+	return session.GetOrCreate(
+		ctx,
+		session.NewParams().
+			WithUserInfo(simulator.DefaultLogin.Username(), password).
+			WithServer(fmt.Sprintf("http://%s", server)).
+			WithDatacenter("*"))
+}
+
+func getPoweredoffVM(ctx goctx.Context, c *vim25.Client) (*object.VirtualMachine, error) {
+	finder := find.NewFinder(c)
+	vm, err := finder.VirtualMachine(ctx, "DC0_H0_VM0")
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = vm.PowerOff(ctx)
+	return vm, err
+}
+
+func storagePolicyModel() (*simulator.Model, error) {
+	model := simulator.VPX()
+	err := model.Create()
+	if err != nil {
+		return nil, err
+	}
+	model.Service.RegisterSDK(pbmsimulator.New())
+	model.Machine = 1
+	model.Datacenter = 1
+	model.Host = 1
+	return model, nil
 }

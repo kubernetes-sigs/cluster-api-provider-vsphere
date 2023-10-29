9cec76143d92355bac9f2df261029ddf880bef4c
Maxim Snezhkov <m.snezhkov@gmail.com>
2023-10-29 15:15:32 +0700

    Add flag to reserve memory to the virtual machine memory size

--- pkg/services/govmomi/vcenter/clone.go
+++ pkg/services/govmomi/vcenter/clone.go
@@ -167,6 +167,12 @@ func Clone(ctx context.Context, vmCtx *capvcontext.VMContext, bootstrapData []by
 		memMiB = 2048
 	}
 
+	var memLockedToMax *bool
+	if vmCtx.VSphereVM.Spec.MemoryReservationLockedToMax != nil {
+		memLockedToMax = new(bool)
+		*memLockedToMax = *vmCtx.VSphereVM.Spec.MemoryReservationLockedToMax
+	}
+
 	// Disable the vAppConfig during VM creation to ensure Cloud-Init inside of the guest does not
 	// activate and prefer the OVF datasource over the VMware datasource.
 	vappConfigRemoved := true
@@ -176,14 +182,15 @@ func Clone(ctx context.Context, vmCtx *capvcontext.VMContext, bootstrapData []by
 			// Assign the clone's InstanceUUID the value of the Kubernetes Machine
 			// object's UID. This allows lookup of the cloned VM prior to knowing
 			// the VM's UUID.
-			InstanceUuid:      string(vmCtx.VSphereVM.UID),
-			Flags:             newVMFlagInfo(),
-			DeviceChange:      deviceSpecs,
-			ExtraConfig:       extraConfig,
-			NumCPUs:           numCPUs,
-			NumCoresPerSocket: numCoresPerSocket,
-			MemoryMB:          memMiB,
-			VAppConfigRemoved: &vappConfigRemoved,
+			InstanceUuid:                 string(vmCtx.VSphereVM.UID),
+			Flags:                        newVMFlagInfo(),
+			DeviceChange:                 deviceSpecs,
+			ExtraConfig:                  extraConfig,
+			NumCPUs:                      numCPUs,
+			NumCoresPerSocket:            numCoresPerSocket,
+			MemoryMB:                     memMiB,
+			MemoryReservationLockedToMax: memLockedToMax,
+			VAppConfigRemoved:            &vappConfigRemoved,
 		},
 		Location: types.VirtualMachineRelocateSpec{
 			DiskMoveType: string(diskMoveType),

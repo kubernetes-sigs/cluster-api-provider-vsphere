486224242437b2f5fefb9651fac5684f4b6fd81c
Christian Schlotter <christian.schlotter@broadcom.com>
2024-04-29 09:51:35 +0200

    Prepare main for development of release v1.11

--- test/e2e/clusterctl_upgrade_test.go
+++ test/e2e/clusterctl_upgrade_test.go
@@ -35,18 +35,16 @@ var (
 	capvReleaseMarkerPrefix = "go://sigs.k8s.io/cluster-api-provider-vsphere@v%s"
 )
 
-var _ = Describe("When testing clusterctl upgrades using ClusterClass (CAPV 1.9=>current, CAPI 1.6=>1.7) [ClusterClass]", func() {
-	const specName = "clusterctl-upgrade-1.9-current" // prefix (clusterctl-upgrade) copied from CAPI
+var _ = Describe("When testing clusterctl upgrades using ClusterClass (CAPV 1.10=>current, CAPI 1.7=>1.7) [ClusterClass]", func() {
+	const specName = "clusterctl-upgrade-1.10-current" // prefix (clusterctl-upgrade) copied from CAPI
 	Setup(specName, func(testSpecificSettingsGetter func() testSettings) {
 		capi_e2e.ClusterctlUpgradeSpec(ctx, func() capi_e2e.ClusterctlUpgradeSpecInput {
-			// Get CAPI v1.6 latest stable release
-			capiVersion16 := "1.6"
-			capiStableRelease16, err := getStableReleaseOfMinor(ctx, capiReleaseMarkerPrefix, capiVersion16)
-			Expect(err).ToNot(HaveOccurred(), "Failed to get stable version for minor release : %s", capiVersion16)
-			// Get CAPV v1.9 latest stable release
-			capvVersion19 := "1.9"
-			capvStableRelease19, err := getStableReleaseOfMinor(ctx, capvReleaseMarkerPrefix, capvVersion19)
-			Expect(err).ToNot(HaveOccurred(), "Failed to get stable version for minor release : %s", capvVersion19)
+			capiVersion := "1.7"
+			capiStableRelease, err := getStableReleaseOfMinor(ctx, capiReleaseMarkerPrefix, capiVersion)
+			Expect(err).ToNot(HaveOccurred(), "Failed to get stable version for minor release : %s", capiVersion)
+			capvVersion := "1.10"
+			capvStableRelease, err := getStableReleaseOfMinor(ctx, capvReleaseMarkerPrefix, capvVersion)
+			Expect(err).ToNot(HaveOccurred(), "Failed to get stable version for minor release : %s", capvVersion)
 			return capi_e2e.ClusterctlUpgradeSpecInput{
 				E2EConfig:                         e2eConfig,
 				ClusterctlConfigPath:              testSpecificSettingsGetter().ClusterctlConfigPath,
@@ -55,11 +53,47 @@ var _ = Describe("When testing clusterctl upgrades using ClusterClass (CAPV 1.9=
 				SkipCleanup:                       skipCleanup,
 				MgmtFlavor:                        testSpecificSettingsGetter().FlavorForMode("remote-management"),
 				PostNamespaceCreated:              testSpecificSettingsGetter().PostNamespaceCreatedFunc,
-				InitWithBinary:                    fmt.Sprintf(clusterctlDownloadURL, capiStableRelease16),
-				InitWithCoreProvider:              fmt.Sprintf(providerCAPIPrefix, capiStableRelease16),
-				InitWithBootstrapProviders:        []string{fmt.Sprintf(providerKubeadmPrefix, capiStableRelease16)},
-				InitWithControlPlaneProviders:     []string{fmt.Sprintf(providerKubeadmPrefix, capiStableRelease16)},
-				InitWithInfrastructureProviders:   []string{fmt.Sprintf(providerVSpherePrefix, capvStableRelease19)},
+				InitWithBinary:                    fmt.Sprintf(clusterctlDownloadURL, capiStableRelease),
+				InitWithCoreProvider:              fmt.Sprintf(providerCAPIPrefix, capiStableRelease),
+				InitWithBootstrapProviders:        []string{fmt.Sprintf(providerKubeadmPrefix, capiStableRelease)},
+				InitWithControlPlaneProviders:     []string{fmt.Sprintf(providerKubeadmPrefix, capiStableRelease)},
+				InitWithInfrastructureProviders:   []string{fmt.Sprintf(providerVSpherePrefix, capvStableRelease)},
+				InitWithRuntimeExtensionProviders: []string{},
+				InitWithIPAMProviders:             []string{},
+				// InitWithKubernetesVersion should be the highest kubernetes version supported by the init Cluster API version.
+				// This is to guarantee that both, the old and new CAPI version, support the defined version.
+				// Ensure all Kubernetes versions used here are covered in patch-vsphere-template.yaml
+				InitWithKubernetesVersion: "v1.30.0",
+				WorkloadKubernetesVersion: "v1.30.0",
+				WorkloadFlavor:            testSpecificSettingsGetter().FlavorForMode("workload"),
+			}
+		})
+	}, WithIP("WORKLOAD_CONTROL_PLANE_ENDPOINT_IP"))
+})
+
+var _ = Describe("When testing clusterctl upgrades using ClusterClass (CAPV 1.9=>current, CAPI 1.6=>1.7) [ClusterClass]", func() {
+	const specName = "clusterctl-upgrade-1.9-current" // prefix (clusterctl-upgrade) copied from CAPI
+	Setup(specName, func(testSpecificSettingsGetter func() testSettings) {
+		capi_e2e.ClusterctlUpgradeSpec(ctx, func() capi_e2e.ClusterctlUpgradeSpecInput {
+			capiVersion := "1.6"
+			capiStableRelease, err := getStableReleaseOfMinor(ctx, capiReleaseMarkerPrefix, capiVersion)
+			Expect(err).ToNot(HaveOccurred(), "Failed to get stable version for minor release : %s", capiVersion)
+			capvVersion := "1.9"
+			capvStableRelease, err := getStableReleaseOfMinor(ctx, capvReleaseMarkerPrefix, capvVersion)
+			Expect(err).ToNot(HaveOccurred(), "Failed to get stable version for minor release : %s", capvVersion)
+			return capi_e2e.ClusterctlUpgradeSpecInput{
+				E2EConfig:                         e2eConfig,
+				ClusterctlConfigPath:              testSpecificSettingsGetter().ClusterctlConfigPath,
+				BootstrapClusterProxy:             bootstrapClusterProxy,
+				ArtifactFolder:                    artifactFolder,
+				SkipCleanup:                       skipCleanup,
+				MgmtFlavor:                        testSpecificSettingsGetter().FlavorForMode("remote-management"),
+				PostNamespaceCreated:              testSpecificSettingsGetter().PostNamespaceCreatedFunc,
+				InitWithBinary:                    fmt.Sprintf(clusterctlDownloadURL, capiStableRelease),
+				InitWithCoreProvider:              fmt.Sprintf(providerCAPIPrefix, capiStableRelease),
+				InitWithBootstrapProviders:        []string{fmt.Sprintf(providerKubeadmPrefix, capiStableRelease)},
+				InitWithControlPlaneProviders:     []string{fmt.Sprintf(providerKubeadmPrefix, capiStableRelease)},
+				InitWithInfrastructureProviders:   []string{fmt.Sprintf(providerVSpherePrefix, capvStableRelease)},
 				InitWithRuntimeExtensionProviders: []string{},
 				InitWithIPAMProviders:             []string{},
 				// InitWithKubernetesVersion should be the highest kubernetes version supported by the init Cluster API version.
@@ -67,44 +101,6 @@ var _ = Describe("When testing clusterctl upgrades using ClusterClass (CAPV 1.9=
 				// Ensure all Kubernetes versions used here are covered in patch-vsphere-template.yaml
 				InitWithKubernetesVersion: "v1.29.0",
 				WorkloadKubernetesVersion: "v1.29.0",
-				WorkloadFlavor:            testSpecificSettingsGetter().FlavorForMode("workload"),
-			}
-		})
-	}, WithIP("WORKLOAD_CONTROL_PLANE_ENDPOINT_IP"))
-})
-
-var _ = Describe("When testing clusterctl upgrades using ClusterClass (CAPV 1.8=>current, CAPI 1.5=>1.7) [ClusterClass]", func() {
-	const specName = "clusterctl-upgrade-1.8-current" // prefix (clusterctl-upgrade) copied from CAPI
-	Setup(specName, func(testSpecificSettingsGetter func() testSettings) {
-		capi_e2e.ClusterctlUpgradeSpec(ctx, func() capi_e2e.ClusterctlUpgradeSpecInput {
-			// Get CAPI v1.5 latest stable release
-			capiVersion15 := "1.5"
-			capiStableRelease15, err := getStableReleaseOfMinor(ctx, capiReleaseMarkerPrefix, capiVersion15)
-			Expect(err).ToNot(HaveOccurred(), "Failed to get stable version for minor release : %s", capiVersion15)
-			// Get CAPV v1.8 latest stable release
-			capvVersion18 := "1.8"
-			capvStableRelease18, err := getStableReleaseOfMinor(ctx, capvReleaseMarkerPrefix, capvVersion18)
-			Expect(err).ToNot(HaveOccurred(), "Failed to get stable version for minor release : %s", capvVersion18)
-			return capi_e2e.ClusterctlUpgradeSpecInput{
-				E2EConfig:                         e2eConfig,
-				ClusterctlConfigPath:              testSpecificSettingsGetter().ClusterctlConfigPath,
-				BootstrapClusterProxy:             bootstrapClusterProxy,
-				ArtifactFolder:                    artifactFolder,
-				SkipCleanup:                       skipCleanup,
-				MgmtFlavor:                        testSpecificSettingsGetter().FlavorForMode("remote-management"),
-				PostNamespaceCreated:              testSpecificSettingsGetter().PostNamespaceCreatedFunc,
-				InitWithBinary:                    fmt.Sprintf(clusterctlDownloadURL, capiStableRelease15),
-				InitWithCoreProvider:              fmt.Sprintf(providerCAPIPrefix, capiStableRelease15),
-				InitWithBootstrapProviders:        []string{fmt.Sprintf(providerKubeadmPrefix, capiStableRelease15)},
-				InitWithControlPlaneProviders:     []string{fmt.Sprintf(providerKubeadmPrefix, capiStableRelease15)},
-				InitWithInfrastructureProviders:   []string{fmt.Sprintf(providerVSpherePrefix, capvStableRelease18)},
-				InitWithRuntimeExtensionProviders: []string{},
-				InitWithIPAMProviders:             []string{},
-				// InitWithKubernetesVersion should be the highest kubernetes version supported by the init Cluster API version.
-				// This is to guarantee that both, the old and new CAPI version, support the defined version.
-				// Ensure all Kubernetes versions used here are covered in patch-vsphere-template.yaml
-				InitWithKubernetesVersion: "v1.28.0",
-				WorkloadKubernetesVersion: "v1.28.0",
 				WorkloadFlavor:            testSpecificSettingsGetter().FlavorForMode("workload"),
 			}
 		})

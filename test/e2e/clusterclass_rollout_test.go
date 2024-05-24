8e8e1f8bb1c07caba43022fa82dba3049402b96a
Gong Zhang <gongz@vmware.com>
2024-05-24 14:30:29 +0800

    Fill up supervisor e2e test
    - Enable test "When testing ClusterClass rollouts"
    
    Signed-off-by: Gong Zhang <gongz@vmware.com>

---
+++ test/e2e/clusterclass_rollout_test.go
@@ -0,0 +1,41 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	"k8s.io/utils/ptr"
+	capie2e "sigs.k8s.io/cluster-api/test/e2e"
+)
+
+var _ = Describe("When testing ClusterClass rollouts [supervisor] [ClusterClass]", func() {
+	const specName = "clusterclass-rollouts" // copied from CAPI
+	Setup(specName, func(testSpecificSettingsGetter func() testSettings) {
+		capie2e.ClusterClassRolloutSpec(ctx, func() capie2e.ClusterClassRolloutSpecInput {
+			return capie2e.ClusterClassRolloutSpecInput{
+				E2EConfig:              e2eConfig,
+				ClusterctlConfigPath:   testSpecificSettingsGetter().ClusterctlConfigPath,
+				BootstrapClusterProxy:  bootstrapClusterProxy,
+				ArtifactFolder:         artifactFolder,
+				SkipCleanup:            skipCleanup,
+				Flavor:                 testSpecificSettingsGetter().FlavorForMode("topology"),
+				PostNamespaceCreated:   testSpecificSettingsGetter().PostNamespaceCreatedFunc,
+				InfrastructureProvider: ptr.To("vsphere"),
+			}
+		})
+	})
+})

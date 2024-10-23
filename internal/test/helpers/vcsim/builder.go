af6488cd95bbc373c77e5797b130c7d0f46f026b
Christian Schlotter <christian.schlotter@broadcom.com>
2024-10-23 16:39:54 +0200

    Bump govmomi to v0.45.1

--- internal/test/helpers/vcsim/builder.go
+++ internal/test/helpers/vcsim/builder.go
@@ -17,16 +17,24 @@ limitations under the License.
 package vcsim
 
 import (
+	"crypto/rand"
 	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
 	"fmt"
+	"math/big"
+	"net"
 	"net/url"
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/onsi/gomega/gbytes"
+	"github.com/pkg/errors"
 	"github.com/vmware/govmomi/simulator"
 	_ "github.com/vmware/govmomi/vapi/cluster/simulator" // import this to register cluster module service test endpoint
+	"sigs.k8s.io/cluster-api/util/certs"
 )
 
 // Builder helps in creating a vcsim simulator.
@@ -81,6 +89,12 @@ func (b *Builder) Build() (*Simulator, error) {
 	}
 
 	b.model.Service.TLS = new(tls.Config)
+	keyPair, err := generateTLSKeyPair()
+	if err != nil {
+		return nil, err
+	}
+
+	b.model.Service.TLS.Certificates = append(b.model.Service.TLS.Certificates, keyPair)
 	b.model.Service.RegisterEndpoints = true
 	server := b.model.Service.NewServer()
 	simr := &Simulator{
@@ -118,4 +132,48 @@ func govcCommand(govcURL, commandStr string, buffers ...*gbytes.Buffer) *exec.Cm
 		cmd.Stderr = buffers[1]
 	}
 	return cmd
+}
+
+// generateTLSKeyPair generates a self-signed certificatge keypair similar to the
+// hardcoded values in github.com/vmware/govmomi/simulator/internal but adds the
+// POD_IP to the IP Addresses of the certificate.
+func generateTLSKeyPair() (tls.Certificate, error) {
+	privateKey, err := certs.NewPrivateKey()
+	if err != nil {
+		return tls.Certificate{}, err
+	}
+
+	template := x509.Certificate{
+		SerialNumber: new(big.Int).SetInt64(0),
+		Subject: pkix.Name{
+			Organization: []string{"CAPV vcsim"},
+		},
+		NotBefore: time.Now().Add(time.Minute * -5),
+		NotAfter:  time.Now().Add(time.Hour * 24 * 365),
+
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment | x509.KeyUsageCertSign,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+		IsCA:                  true,
+		IPAddresses: []net.IP{
+			net.ParseIP("127.0.0.1"),
+			net.ParseIP("::1"),
+		},
+	}
+
+	if ip := os.Getenv("POD_IP"); ip != "" {
+		template.IPAddresses = append(template.IPAddresses, net.ParseIP(ip))
+	}
+
+	b, err := x509.CreateCertificate(rand.Reader, &template, &template, &privateKey.PublicKey, privateKey)
+	if err != nil {
+		return tls.Certificate{}, errors.Wrap(err, "failed to create certificate")
+	}
+
+	cert, err := x509.ParseCertificate(b)
+	if err != nil {
+		return tls.Certificate{}, errors.Wrap(err, "failed to parse certificate")
+	}
+
+	return tls.X509KeyPair(certs.EncodeCertPEM(cert), certs.EncodePrivateKeyPEM(privateKey))
 }
